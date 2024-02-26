bc464956deac1b23e00f441eef2f9f845e30bf1f
DeltaScratchPad <aaverell@gmail.com>
2024-02-26 20:45:15 +0000

    Added support interfaces

--- error.go
+++ error.go
@@ -1,5 +1,7 @@
 package go_system_api
 
+import "encoding/json"
+
 type ErrorBody struct {
 	QueryID     string     `json:"query_id"`
 	Step        int64      `json:"step"`
@@ -7,4 +9,16 @@ type ErrorBody struct {
 	ErrorMsg    string     `json:"error_msg"`
 	DebugMsg    string     `json:"debug_msg"`
 	Event       *EventData `json:"event"`
+}
+
+func (e ErrorBody) String() string {
+	bytes, err := json.Marshal(e)
+	if err != nil {
+		panic("Failed to serialise.")
+	}
+	return string(bytes)
+}
+
+func (e ErrorBody) Error() string {
+	return e.ErrorMsg
 }
